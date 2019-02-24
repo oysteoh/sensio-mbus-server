c35da4ac650e85410f27410cbcb026ee244a7ce9
Øystein Olai Heggen <oystein.heggen@gmail.com>
2019-02-24 11:51:27 +0100

    added server

---
+++ server/server.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import logging
+from socketserver import TCPServer
+from collections import defaultdict
+
+from umodbus import conf
+from umodbus.server.tcp import RequestHandler, get_server
+from umodbus.utils import log_to_stream
+
+config = {
+    'log_level': logging.DEBUG,
+    'signed_values': True,
+    'ip-address': '192.168.1.122',
+    'port': 1502,
+    'data-file': '/home/pi/data.txt',
+    'value-separator': ";",
+    'address-range': list(range(0, 10))
+}
+
+# Add stream handler to logger 'uModbus'.
+log_to_stream(level=config['log_level'])
+
+# Enable values to be signed (default is False).
+conf.SIGNED_VALUES = config['signed_values']
+
+TCPServer.allow_reuse_address = True
+app = get_server(TCPServer, (config['ip-address'], config['port']), RequestHandler)
+
+
+@app.route(slave_ids=[1], function_codes=[3, 4], addresses=config['address-range'])
+def read_data_store(slave_id, function_code, address):
+    """" Return value of address. """
+    data = _read_file()
+    return int(data[address])
+
+@app.route(slave_ids=[1], function_codes=[6, 16], addresses=config['address-range'])
+def write_data_store(slave_id, function_code, address, value):
+    """" Set value for address. """
+    data = _read_file()    
+    data[address] = value
+    _write_file(data)
+
+def _read_file():
+    with open(config['data-file'], "r") as data_file:
+        return data_file.read().split(config['value_separator'])   
+     
+def _write_file(data):
+    with open(config['data-file'], "w") as data_file:
+        data_file.write(config['value_separator'].join(map(str, data)))
+
+if __name__ == '__main__':
+    app.serve_forever()
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from os import path
 from io import open
@@ -35,46 +36,11 @@ setup(
 
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
  
-    install_requires=['umodbus'],
+    install_requires=['socketserver', 'umodbus'],
 
-    extras_require={  
-        'dev': ['pytest'],
-        'test': ['pytest'],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
+    entry_points={  
         'console_scripts': [
-            'sample=sample:main',
+            'sensio-mbus-server=server:main',
         ],
-    },
-
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
-        'Funding': 'https://donate.pypi.org',
-        'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/pypa/sampleproject/',
-    },
+    }
 )
